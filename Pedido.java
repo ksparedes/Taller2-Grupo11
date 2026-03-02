d16a90ecbd9cd3fc3fdc013e16030c39303d3643
Box109 <cbassa@uninorte.edu.co>
2026-03-02 08:22:10 -0500

    Update Pedido.java

--- Pedido.java
+++ Pedido.java
@@ -1,16 +1,29 @@
-public class Listapedido {
+public class id_pedido {
     private int id_pedido;
+    private int id_cliente;
     private String producto;
     private int precio;
     private int cantidad;
+    private int activo;
 
 
-    public Usuario(int id, String nombre, String email) {
-        this.id = id_pedido;
-        this.producto=producto;
+    public Usuario(int id_pedido, int id_cliente, String producto, double precio, int cantidad, int activo) {
+        this.id_pedido = id_pedido;
+        this.producto = producto;
+        this.id_cliente = id_cliente;
+        this.activo = activo;
         this.precio=precio;
         this.cantidad=cantidad;
     }
+    public int getId_cliente(){
+        return id_cliente;
+    }
+    public int getActivo(){
+        return activo;
+    } 
+    public void setActivo(int Activo){
+        this.activo = activo;
+    }    
 
     @Override
     public String toString() {
